63b48d8ff0ee493e9c92f725b32785c931a871ad
GustavoGJesus <ggomesdejesus01@gmail.com>
2023-03-13 23:26:45 -0300

    feat: create styles

---
+++ src/styles.js
@@ -0,0 +1,61 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+  font-family: "Poppins", sans-serif;
+  text-align: center;
+`;
+
+export const Main = styled.div` 
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+  gap: 10px;
+`
+
+export const ContainerForm = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 20px
+`;
+
+export const FormLabel = styled.label`
+  font-size: 25px;
+  display: flex;
+  flex-direction: column;
+  gap: 05px;
+  margin-top: 20px;
+
+  font-weight: 500;
+`;
+
+export const FormInput = styled.input`
+  padding: 10px;
+  width: 300px;
+  height: 20px;
+
+  border-radius: 10px;
+  outline: none;
+  border: none;
+
+  font-weight: 600;
+  font-size: 18px;
+`;
+
+export const Button = styled.input`
+  border: none;
+  padding: 10px;
+  width: 200px;
+  margin-top: 20px;
+
+  font-weight: 600;
+  font-size: 18px;
+
+  border-radius: 20px;
+  cursor: pointer;
+
+`;
